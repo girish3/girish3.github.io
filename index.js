000b429a3bab6cfc8a65a0fdf739e1dce722bd30
girish <girishiitj@gmail.com>
2023-07-06 16:15:45 +0530

    added mixpanel events

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const joinButton = document.querySelector(".join-button");
 const GOOGLE_FORM_URL = "https://docs.google.com/forms/d/1gmQf3Jbx6NbkwCxVzDbdqcVATJ0UrGI6ZFbeMYm1p-8"
+const t = "5ef97046932e8d79a73de3601fea905d"
+let hoverEventSent = false;
 
-joinButton.addEventListener('click', openForm);
 function openForm() {
-    console.log("Open waitlist form");
     // open webpage on a new tab
     window.open(GOOGLE_FORM_URL, "_blank");
+    mixpanel.track("landing_page_signup_clicked");
 }
+
+// on page load
+window.onload = function() {
+    mixpanel.init(t, { debug: false, track_pageview: false });
+    mixpanel.track("landing_page_visited");
+    const joinButton = document.querySelector(".join-button");
+    joinButton.addEventListener('click', openForm);
+    joinButton.addEventListener('mouseover', function() {        
+        if (!hoverEventSent) {
+            mixpanel.track("landing_page_signup_hovered");
+            hoverEventSent = true;
+        }
+    });
+};
+
